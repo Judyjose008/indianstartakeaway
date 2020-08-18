d90b04bb2b128b8de54f67ac9d5094b10a61209f
judyjose008 <judyjose008@gmail.com>
2020-08-17 21:47:22 -0700

    Regular automated push

--- js/main.js
+++ js/main.js
@@ -46,7 +46,9 @@
 	    autoplay: true,
 	    margin:0,
 	    animateOut: 'fadeOut',
-	    animateIn: 'fadeIn',
+		animateIn: 'fadeIn',git
+		mouseDrag: false,
+		touchDrag: true,
 	    nav:true,
 	    dots: false,
 	    autoplayHoverPause: false,
